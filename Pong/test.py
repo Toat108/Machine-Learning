202309ce632f34d1ff6f406a8e76dcf10b0f1946
Toat <100681162+Toat108@users.noreply.github.com>
2025-03-29 01:45:24 +0900

    Delete Pong directory

--- Pong/test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-print("PyTorch Version:", torch.__version__)
-print("CUDA Available:", torch.cuda.is_available())
-print("CUDA Device Count:", torch.cuda.device_count())
-print("CUDA Device Name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU detected")
-print("Torch CUDA Version:", torch.version.cuda)
